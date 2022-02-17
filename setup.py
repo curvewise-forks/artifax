0d3cae41b4423939f2cc928074dda06a1f0aa4d1
Paul Melnikow <email@paulmelnikow.com>
2022-02-17 10:16:53 -0500

    Fix setup.py when installing from VCS

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 from os import path
 
-with open("requirements.txt") as handle:
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, "requirements.txt")) as handle:
     requirements = handle.readlines()
 
-this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
